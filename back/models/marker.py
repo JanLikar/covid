ea1d94ad90557f9c3f7b097d9c69cc1aa6a34a9d
Jan Likar <jan.likar@protonmail.com>
2020-03-11 11:24:49 +0100

    Make all columns non-nullable

--- back/models/marker.py
+++ back/models/marker.py
@@ -15,10 +15,10 @@ from .meta import Base
 class Marker(Base):
     __tablename__ = 'markers'
     id = Column(Integer, primary_key=True)
-    longitude = Column(Numeric)
-    latitude = Column(Numeric)
-    name = Column(Text(length=50))
-    note = Column(Text(length=500), default='')
-    reported_date = Column(Date)
-    created = Column(DateTime(timezone=True), default=func.now())
-    updated = Column(DateTime(timezone=True), onupdate=func.now())
+    longitude = Column(Numeric, nullable=False)
+    latitude = Column(Numeric, nullable=False)
+    name = Column(Text(length=50), nullable=False)
+    note = Column(Text(length=500), default='', nullable=False)
+    reported_date = Column(Date, nullable=False)
+    created = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    updated = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
