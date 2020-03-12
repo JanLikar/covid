f17a07e4997e9ce47f9fc4faad8e9da39badb8c5
Gabrijel Peršin <gabrijel.persin@termitnjak.si>
2020-03-12 01:26:05 +0000

    Adding security HTTPForbiden to views without login

--- back/views/default.py
+++ back/views/default.py
@@ -18,19 +18,19 @@ def get_iso_date():
 
 
 def parse_iso_date(date):
-	return datetime.datetime.strptime(date, '%Y-%m-%d').date()
+    return datetime.datetime.strptime(date, '%Y-%m-%d').date()
 
 
 def marker_to_dict(marker, user_id):
-	return {
-		'id': marker.id,
-		'lon': float(marker.longitude),
-		'lat': float(marker.latitude),
-		'name': marker.name,
-		'note': marker.note,
-		'reported_date': marker.reported_date.strftime('%Y-%m-%d'),
-		'owned': marker.user_id == user_id,
-	}
+    return {
+        'id': marker.id,
+        'lon': float(marker.longitude),
+        'lat': float(marker.latitude),
+        'name': marker.name,
+        'note': marker.note,
+        'reported_date': marker.reported_date.strftime('%Y-%m-%d'),
+        'owned': marker.user_id == user_id,
+    }
 
 
 @view_config(route_name='home', renderer='../templates/index.jinja2')
@@ -82,53 +82,68 @@ def home_post(request):
 
 @view_config(route_name='add_marker', renderer='../templates/add_marker.jinja2')
 def add_marker(request):
+    if not request.authenticated_userid:
+        raise HTTPForbidden()
+
     return {'isotoday': get_iso_date()}
 
 
 @view_config(route_name='add_marker', xhr=True, renderer='json', request_method='POST')
 def add_marker_post(request):
-	latitude = request.params.get('lat')
-	longitude = request.params.get('lon')
-	name = request.params.get('name')
-	note = request.params.get('note')
-	reported_date = parse_iso_date(request.params.get('reported_date'))
 
-	new_marker = models.Marker(
-		latitude=latitude,
-		longitude=longitude,
-		name=name,
-		note=note,
-		reported_date=reported_date,
-		user_id=request.authenticated_userid,
-	)
+    if not request.authenticated_userid:
+        raise HTTPForbidden()
 
-	request.dbsession.add(new_marker)
-	# Needed to get the ID
-	request.dbsession.flush()
 
-	return marker_to_dict(new_marker, request.authenticated_userid)
+    latitude = request.params.get('lat')
+    longitude = request.params.get('lon')
+    name = request.params.get('name')
+    note = request.params.get('note')
+    reported_date = parse_iso_date(request.params.get('reported_date'))
+
+    new_marker = models.Marker(
+        latitude=latitude,
+        longitude=longitude,
+        name=name,
+        note=note,
+        reported_date=reported_date,
+        user_id=request.authenticated_userid,
+    )
+
+    request.dbsession.add(new_marker)
+    # Needed to get the ID
+    request.dbsession.flush()
+
+    return marker_to_dict(new_marker, request.authenticated_userid)
 
 
 @view_config(route_name='remove_marker', xhr=True, renderer='json')
 def remove_marker(request):
-	marker_id = int(request.matchdict.get('marker_id'))
-	request.dbsession.query(models.Marker).filter_by(
-		id=marker_id,
-		user_id=request.authenticated_userid,
-	).delete()
+    marker_id = int(request.matchdict.get('marker_id'))
+    request.dbsession.query(models.Marker).filter_by(
+        id=marker_id,
+        user_id=request.authenticated_userid,
+    ).delete()
 
-	return {}
+    return {}
 
 
 @view_config(route_name='list_markers', renderer='json')
 def list_markers(request):
-	markers = []
+    markers = []
 
-	for m in request.dbsession.query(models.Marker):
-		markers.append(marker_to_dict(m, request.authenticated_userid))
+    if request.authenticated_userid:
+        db_markers = request.dbsession.query(models.Marker).filter_by(
+            user_id=request.authenticated_userid,
+        )
+    else:
+        db_markers = request.dbsession.query(models.Marker)
+
+    for m in db_markers:
+        markers.append(marker_to_dict(m, request.authenticated_userid))
 
 
-	return markers
+    return markers
 
 @view_config(route_name='logout')
 def logout(request):
