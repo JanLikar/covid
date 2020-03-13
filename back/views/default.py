dc9665cb56bcd55e8b1bd584f4debf8efdf3065d
Jan Likar <jan.likar@protonmail.com>
2020-03-13 20:26:35 +0100

    Test Sentry

--- back/views/default.py
+++ back/views/default.py
@@ -35,6 +35,7 @@ def marker_to_dict(marker, user_id):
 
 @view_config(route_name='home', renderer='../templates/index.jinja2')
 def home(request):
+    raise ValueError()
     return {'isotoday': get_iso_date(),
             'gen_passphrase': get_passphrase()}
 
