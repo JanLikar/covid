eddec47aa1a7030d695b4138e19808efd10b1a4f
Jan Likar <jan.likar@protonmail.com>
2020-03-14 20:07:58 +0100

    Change passphrase length to 4 words

--- back/models/user.py
+++ back/models/user.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
     Column,
     Integer,
     Text,
+    UniqueConstraint,
 )
 
 from .meta import Base
@@ -10,6 +11,10 @@ from .meta import Base
 
 class User(Base):
     __tablename__ = 'users'
+    __table_args__ = (
+        UniqueConstraint('passphrase', name='passphrase_uc'),
+    )
+
     id = Column(Integer, primary_key=True)
     passphrase = Column(Text(), nullable=False)
     email = Column(Text())
--- back/utils.py
+++ back/utils.py
@@ -1,15 +1,9 @@
 import diceware
 import argparse
 from . import models
-def get_passphrase(request):
 
-    options = diceware.handle_options(['--num', '3'])
 
-    dupes = ['fake_pass']
-    while len(dupes) > 0:
-        passphrase = diceware.get_passphrase(options)
-        # Check if already exists
-        dupes = request.dbsession.query(models.User).filter_by(
-            passphrase=passphrase).all()
+def get_passphrase():
+    options = diceware.handle_options(['--num', '4'])
 
-    return passphrase
+    return diceware.get_passphrase(options)
